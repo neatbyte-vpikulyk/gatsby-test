2fb46402d4d6628bf86c3b8e4ce802cb36300c08
ViktorPikulyk <viktor.pikulyk@neatbyte.solutions>
2022-01-18 12:30:24 +0200

    Added about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-// Step 1: Import React
-import * as React from 'react'
-// Step 2: Define your component
+import * as React from "react";
+import { Link } from "gatsby";
+
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
-  )
-}
-// Step 3: Export your component
-export default IndexPage
+  );
+};
+
+export default IndexPage;
